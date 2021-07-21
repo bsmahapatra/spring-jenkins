320dd7651f13431865dc3380664357de1bc0c706
bsmahapatra <java2010.bsm@gmail.com>
2021-07-21 19:03:19 +0530

    first commit

---
+++ src/test/java/com/jenkins/in/SpringJenkinsApplicationTests.java
@@ -0,0 +1,21 @@
+package com.jenkins.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+		assertEquals(true, true);
+		
+	}
+
+	private void assertEquals(boolean b, boolean c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
